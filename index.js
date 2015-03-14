ab9cad50c22ed473f9f09c402e9146f747f098a8
Ryan Ogle <ryan@zefti.com>
2015-03-13 20:47:07 -0700

    stuff

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 var mongo = require('zefti-mongo');
 var redis = require('zefti-redis');
+var remote = require('zefti-remote');
 
 var originSources = {
     mongo : mongo
   , redis : redis
+  , remote : remote
 };
 
 var readySources = {};
 
 module.exports = function(dataSources){
   for (var dataSource in dataSources) {
+    console.log(dataSource);
     if (!readySources[dataSource]) {
       readySources[dataSource] = originSources[dataSources[dataSource].type](dataSources[dataSource])
     }
   }
+  console.log(readySources.config);
   return readySources;
 }
