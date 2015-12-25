cb1f1d1e1d198801f6c563e59a4fadb4b5dae9e1
RYAN OGLE <ryanogle@RYANs-MacBook-Air.local>
2015-12-24 23:12:19 -0800

    stuff

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ var mongo = require('zefti-mongo');
 var redis = require('zefti-redis');
 var remote = require('zefti-remote');
 var dynamo = require('zefti-dynamo');
+var sqs = require('zefti-sqs');
+var redisIoAdapter = require('zefti-redis-io-adapter');
 
 var originSources = {
     dynamo : dynamo
   , mongo : mongo
   , redis : redis
+  , redisIoAdapter : redisIoAdapter
   , remote : remote
+  , sqs : sqs
 };
 
 var readySources = {};
